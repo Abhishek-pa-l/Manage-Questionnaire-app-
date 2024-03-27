630d18f178ee970d0e37c16aa61a14650f5e1923
Abhishek pal <pala@terralink-global.com>
2024-03-27 12:23:24 +0000

    second

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -6,42 +6,47 @@ sap.ui.define([
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller,JSONModel,Fragment) {
+    function (Controller, JSONModel, Fragment) {
         "use strict";
 
         return Controller.extend("com.sap.managequestionnaireapp.controller.View1", {
             onInit: function () {
                 let data = [
                     {
-                        "QuestionId": "ITT24.0001",
-                        "QuestionCatogrie": "catogrie1",
-                        "Active": "true",
+                        "QuestionId": "	27174952",
+                        "Questiontitle": "Query1",
+                        "QuestionCatogrie": "category1",
+                        "Active": "True",
                         "CreatedOn": "1/11/2024",
                         "CreatedBy": "danara",
-                        
+
                     },
                     {
-                        "QuestionId": "ITT24.0002",
-                        "QuestionCatogrie": "catogrie2",
-                        "Active": "true",
+                        "QuestionId": "27174954",
+                        "Questiontitle": "Query2",
+
+                        "QuestionCatogrie": "category2",
+                        "Active": "True",
                         "CreatedOn": "1/11/2024",
                         "CreatedBy": "danara",
-                        
+
                     },
                     {
-                        "QuestionId": "ITT24.0003",
-                        "QuestionCatogrie": "catogrie3",
-                        "Active": "true",
+                        "QuestionId": "27174955",
+                        "Questiontitle": "Query3",
+
+                        "QuestionCatogrie": "category3",
+                        "Active": "True",
                         "CreatedOn": "1/11/2024",
                         "CreatedBy": "danara",
-                        
+
                     },
                 ];
-    
+
                 let jModel = new JSONModel(data);
                 this.getView().setModel(jModel, "jModel");
             },
-            onCreateQuestion : function () {
+            onCreateQuestion: function () {
                 debugger
                 if (!this._QuestionDialog) {
                     Fragment.load({
@@ -62,5 +67,39 @@ sap.ui.define([
                 this._QuestionDialog.close()
 
             },
+            onAdd: function () {
+                let aa = this.getView().byId("questioncatid").getValue();
+                let bb = this.getView().byId("questiontitleid").getValue();
+                let currentDate = new Date();
+                let day = currentDate.getDate();
+                let month = currentDate.getMonth() + 1;
+                let year = currentDate.getFullYear();
+                let hours = currentDate.getHours();
+                let minutes = currentDate.getMinutes();
+                let seconds = currentDate.getSeconds();
+                let QuestionId = day + "" + hours + "" + minutes + "" + seconds
+
+                let date = day + '/' + month + '/' + year;
+                let newData = {
+                    "QuestionId": QuestionId,
+                    "Questiontitle": bb,
+
+                    "QuestionCatogrie": aa,
+                    "Active": "True",
+                    "CreatedOn": date,
+                    "CreatedBy": "Annonymus",
+
+                }
+
+                let mymodel = this.getView().getModel("jModel");
+                let olddata = mymodel.getData();
+                olddata.push(newData)
+                mymodel.setData(olddata);
+                mymodel.refresh();
+                this._QuestionDialog.close()
+                this.getView().byId("questioncatid").setValue("");
+                this.getView().byId("questiontitleid").setValue("");
+
+            }
         });
     });
